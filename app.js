813fad8c43dda172abfb6237ab443c5950e9abd0
rajat643 <rajat_2101ee60@iitp.ac.in>
2023-10-28 18:25:42 +0530

    Error message fixes

--- app.js
+++ app.js
@@ -14,34 +14,39 @@ import participantRoutes from "./routes/participant.route.js";
 const app = express();
 
 const allowedOrigins = [
-	'http://localhost:3000',
-	'https://njath-2023.vercel.app',
-	// Add more origins as needed
+  "http://localhost:3000",
+  "https://njath-2023.vercel.app",
+  // Add more origins as needed
 ];
 
 // -----------MIDDLEWARE-------------
-app.use(cors({
-	origin: allowedOrigins,
-	credentials: true,
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 //  :TODO: Move it somewhere else
 morgan.token("pino-logger", (req, res) => {
-	// all it does is print incoming http request
-	logger.info({ method: req.method, url: req.originalUrl }, "HTTP Request");
-	return "";
+  // all it does is print incoming http request
+  logger.info({ method: req.method, url: req.originalUrl }, "HTTP Request");
+  return "";
 });
 app.use(morgan(":pino-logger"));
-
 
 // -----------ROUTES---------------
 
 app.use("/api/auth", authRouter);
 app.use("/api/question", questionRouter);
-app.use("/api/participant", checkUser([USER_ROLE.PARTICIPANT]), participantRoutes);
+app.use(
+  "/api/participant",
+  checkUser([USER_ROLE.PARTICIPANT]),
+  participantRoutes
+);
 
 // app.use((err, req, res, next) => {
 // 	console.error(err.stack);
@@ -49,15 +54,14 @@ app.use("/api/participant", checkUser([USER_ROLE.PARTICIPANT]), participantRoute
 // });
 
 app.use("*", (req, res, next) => {
-	return res.status(404).json({
-		message: "Not found",
-	});
+  return res.status(404).json({
+    message: "Not found",
+  });
 });
-
 
 // -----------START SERVER------------
 // :TODO: find some better way to start server
 app.listen(config.port, () => {
-	logger.info("🚀 Server is running on port 8080");
+  logger.info("🚀 Server is running on port 8080");
 });
 connectdb();
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,243 +1,247 @@
 import config from "../config/default.mjs";
-import { Organizer, Participant, USER_ROLE, User } from "../models/user.model.js";
-import { getHash, hashPassword, comparePassword, generateJwtToken, sendMail, checkJwtToken } from "../utils/auth.util.js";
+import {
+  Organizer,
+  Participant,
+  USER_ROLE,
+  User,
+} from "../models/user.model.js";
+import {
+  getHash,
+  hashPassword,
+  comparePassword,
+  generateJwtToken,
+  sendMail,
+  checkJwtToken,
+} from "../utils/auth.util.js";
 import logger from "../utils/logger.js";
 
-
 export const registerParticipant = (role) => {
-	return async (req, res) => {
-		const params = req.body;
-		params["password"] = await hashPassword(params["password"]);
-		params["role"] = role;
+  return async (req, res) => {
+    const params = req.body;
+    params["password"] = await hashPassword(params["password"]);
+    params["role"] = role;
 
-		try {
-			let user = null;
+    try {
+      let user = null;
 
-			if (role === USER_ROLE.PARTICIPANT) {
-				user = new Participant(params);
-			}
-			else {
-				user = new Organizer(params);
-			}
+      if (role === USER_ROLE.PARTICIPANT) {
+        user = new Participant(params);
+      } else {
+        user = new Organizer(params);
+      }
 
-			await user.save();
-			return res.status(201).json({
-				success: true,
-				message: "user created succesfully",
-			});
-		} catch (e) {
-			if (e.code === 11000) {
-				// Duplicate key error (MongoDB error code 11000)
-				return res.status(409).json({
-					success: false,
-					message: "some user with same fields already exists",
-				});
-			} else {
-				logger.error(e);
-				return res.status(500).json({
-					success: false,
-					message: "User was not created",
-				});
-			}
-		}
-	};
-}
+      await user.save();
+      return res.status(201).json({
+        success: true,
+        message: "User created succesfully",
+      });
+    } catch (e) {
+      if (e.code === 11000) {
+        return res.status(409).json({
+          success: false,
+          message: "Some user with same fields already exists",
+        });
+      } else {
+        logger.error(e);
+        return res.status(500).json({
+          success: false,
+          message: "User was not created",
+        });
+      }
+    }
+  };
+};
 
 export const login = async (req, res) => {
-	const userData = req.body;
-	try {
-		const user = await User.findOne({ email: userData.email })
+  const userData = req.body;
+  try {
+    const user = await User.findOne({ email: userData.email });
 
-		if (!user) {
-			return res.status(401).json({
-				success: false,
-				message: "Incorrect email"
-			});
-		}
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User with the provided email doesn't exist.",
+      });
+    }
 
-		const match = await comparePassword(userData.password, user.password);
+    const match = await comparePassword(userData.password, user.password);
 
-		if (!match) {
-			return res.status(401).json({
-				success: false,
-				message: "Incorrect password"
-			});
-		}
+    if (!match) {
+      return res.status(401).json({
+        success: false,
+        message: "Incorrect password",
+      });
+    }
 
-		const payload = {
-			email: user.email,
-			username: user.username,
-			phone: user.phone
-		}
+    const payload = {
+      email: user.email,
+      username: user.username,
+      phone: user.phone,
+    };
 
-		const acessToken = generateJwtToken(payload);
+    const acessToken = generateJwtToken(payload);
 
-		res.cookie('access_token', acessToken, {
-			maxAge: 24 * 60 * 60 * 1000,
-			httpOnly: true,
-			sameSite: 'None', // Allow cross-origin requests
-			domain: 'njath-2023.vercel.app', // Set the domain to your frontend's domain
-			path: '/', // The path for which the cookie is valid (root path in this case)
-		});
+    res.cookie("access_token", acessToken, {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: true,
+      sameSite: "Lax", // Controls when the cookie should be sent in cross-origin requests
+      path: "/", // The path for which the cookie is valid (root path in this case)
+    });
 
-		return res.status(200).json({
-			success: true,
-			message: 'Login successful'
-		});
-	}
-	catch (e) {
-		logger.error(e);
-		return res.status(500).json({
-			success: false,
-			message: "Login failed, sesrver error"
-		});
-	}
-
-}
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+    });
+  } catch (e) {
+    logger.error(e);
+    return res.status(500).json({
+      success: false,
+      message: "Login failed, sesrver error",
+    });
+  }
+};
 
 export const forgetPassword = async (req, res) => {
-	const userData = req.body;
-	try {
-		const user = await User.findOne(userData).exec();
+  const userData = req.body;
+  try {
+    const user = await User.findOne(userData).exec();
 
-		if (!user) {
-			return res.status(401).json({
-				success: false,
-				message: "No user with the given data found"
-			})
-		}
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "No user with the given data found",
+      });
+    }
 
-		const payload = {
-			email: user.email,
-			phone: user.phone,
-			username: user.username
-		}
+    const payload = {
+      email: user.email,
+      phone: user.phone,
+      username: user.username,
+    };
 
-		const token = generateJwtToken(payload);
-		const subject = "Reset Password for NJATH"
-		const mailText = `Hello,${user.username}\n\n` +
-			"Please click on the link below to reset your password\n" +
-			`${config.frontendUrl}/reset-password/${token}\n\n` +
-			"Thank you\n\nTeam NJATH"
+    const token = generateJwtToken(payload);
+    const subject = "Reset Password for NJATH";
+    const mailText =
+      `Hello,${user.username}\n\n` +
+      "Please click on the link below to reset your password\n" +
+      `${config.frontendUrl}/reset-password/${token}\n\n` +
+      "Thank you\n\nTeam NJATH";
 
-		let result = false;
-		try {
-			result = await sendMail(user.email, subject, mailText);
-		}
-		catch (e) {
-			logger.error(e);
-			return res.status(500).json({
-				success: false,
-				message: "server error"
-			})
-		}
+    let result = false;
+    try {
+      result = await sendMail(user.email, subject, mailText);
+    } catch (e) {
+      logger.error(e);
+      return res.status(500).json({
+        success: false,
+        message: "server error",
+      });
+    }
 
-		if (!result) {
-			return res.status(500).json({
-				success: false,
-				message: "Mail not sent please provide correct credentials"
-			})
-		}
+    if (!result) {
+      return res.status(500).json({
+        success: false,
+        message: "Mail not sent please provide correct credentials",
+      });
+    }
 
-		return res.status(200).json({
-			success: true,
-			message: "Mail sent , please check your inbox, don't forget to check your spam as well"
-		})
-
-	}
-	catch (e) {
-		logger.error(e);
-		return res.status(500).json({
-			success: false,
-			message: "Action failed ,please try again"
-		})
-	}
-}
+    return res.status(200).json({
+      success: true,
+      message:
+        "Mail sent, please check your inbox, don't forget to check your spam as well",
+    });
+  } catch (e) {
+    logger.error(e);
+    return res.status(500).json({
+      success: false,
+      message: "Action failed, please try again",
+    });
+  }
+};
 
 export const resetPassword = async (req, res) => {
-	const token = req.params.token;
-	const userData = req.body;
+  const token = req.params.token;
+  const userData = req.body;
 
-	if (!token) {
-		return res.status(401).json({
-			success: false,
-			message: "Token not found"
-		})
-	}
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Token not found",
+    });
+  }
 
-	try {
-		const payload = checkJwtToken(token);
-		if (!payload.email || !payload.phone || !payload.username) {
-			return res.status(401).json({
-				success: false,
-				message: "Invalid token"
-			})
-		}
+  try {
+    const payload = checkJwtToken(token);
+    if (!payload.email || !payload.phone || !payload.username) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
 
-		try {
-			const user = await User.findOne({
-				email: payload.email,
-				username: payload.username,
-				phone: payload.phone,
-			})
+    try {
+      const user = await User.findOne({
+        email: payload.email,
+        username: payload.username,
+        phone: payload.phone,
+      });
 
-			if (!user) {
-				res.status(401).json({
-					success: false,
-					message: "Invalid token"
-				})
-			}
+      if (!user) {
+        res.status(401).json({
+          success: false,
+          message: "Invalid token",
+        });
+      }
 
-			userData.password = await hashPassword(userData.password);
+      userData.password = await hashPassword(userData.password);
 
-			user.password = userData.password;
+      user.password = userData.password;
 
-			try {
-				const updatedData = await user.save();
-				return res.status(200).json({
-					success: true,
-					message: "Password succesfully reset"
-				})
-			} catch (e) {
-				logger.error(e);
-				return res.status(500).json({
-					success: false,
-					message: "Password reset failed"
-				})
-			}
-		}
-		catch (e) {
-			logger.error(e);
-			res.status(500).json({
-				success: false,
-				message: "Password reset failed"
-			})
-		}
-	}
-	catch (e) {
-		logger.error(e)
-		return res.status(401).json({
-			success: false,
-			message: "Invalid token"
-		})
-	}
-}
+      try {
+        const updatedData = await user.save();
+        return res.status(200).json({
+          success: true,
+          message: "Password succesfully reset",
+        });
+      } catch (e) {
+        logger.error(e);
+        return res.status(500).json({
+          success: false,
+          message: "Password reset failed",
+        });
+      }
+    } catch (e) {
+      logger.error(e);
+      res.status(500).json({
+        success: false,
+        message: "Password reset failed",
+      });
+    }
+  } catch (e) {
+    logger.error(e);
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
 
 export const logout = (req, res) => {
-	try {
-		res.clearCookie('access_token', {
-			path: '/',
-		});
+  try {
+    res.clearCookie("access_token", {
+      path: "/",
+    });
 
-		return res.status(200).json({
-			success: true,
-			message: 'Logged out successfully',
-		});
-	} catch (error) {
-		logger.error(error);
-		return res.status(500).json({
-			success: false,
-			message: 'Logout failed, server error',
-		});
-	}
+    return res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Logout failed, server error",
+    });
+  }
 };
